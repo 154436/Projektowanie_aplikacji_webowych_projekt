ce5a588d8ef7aa29bd021d3cc39f5fcea6227657
154436 <154436@stud.prz.edu.pl>
2023-04-19 22:50:30 +0200

    Powiązanie User z modelem, wylogowanie, zmiana hasła

--- models.py
+++ models.py
@@ -6,34 +6,36 @@ from django.conf import settings
 
 # Create your models here.
 class Nauczyciel(models.Model):
-    imie = models.CharField(max_length=50)
-    nazwisko = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    haslo = models.TextField(max_length=100)
-    nr_telefonu = models.IntegerField()
-    #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    imie = models.CharField(max_length=50,default='test')
+    nazwisko = models.CharField(max_length=50,default='test')
+    email = models.CharField(max_length=50,default='test')
+    haslo = models.TextField(max_length=100,default='test')
+    nr_telefonu = models.IntegerField(default=1)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,default=1)
 
 class Klasa(models.Model):
     id_wychowawcy = models.ForeignKey(Nauczyciel, on_delete=models.CASCADE)
     nazwa = models.CharField(max_length=20)
 
 class Uczen(models.Model):
-    imie = models.CharField(max_length=50)
-    nazwisko = models.CharField(max_length=50)
-    adres_zamieszkania = models.CharField(max_length=150)
-    pesel = models.IntegerField()
-    email = models.CharField(max_length=50)
-    haslo = models.TextField(max_length=100)
-    klasa = models.ForeignKey(Klasa, on_delete=models.CASCADE)
-    #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    imie = models.CharField(max_length=50,default='test')
+    nazwisko = models.CharField(max_length=50,default='test')
+    adres_zamieszkania = models.CharField(max_length=150,default='test')
+    pesel = models.IntegerField(default=1)
+    email = models.CharField(max_length=50,default='test')
+    haslo = models.TextField(max_length=100,default='test')
+    klasa = models.ForeignKey(Klasa, on_delete=models.CASCADE,default=1)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
 class Rodzic(models.Model):
-    imie = models.CharField(max_length=50)
-    nazwisko = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    haslo = models.TextField(max_length=100)
-    nr_telefonu = models.IntegerField()
-    #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    imie = models.CharField(max_length=50,default='test')
+    nazwisko = models.CharField(max_length=50,default='test')
+    email = models.EmailField(max_length=50,default='test')
+    haslo = models.TextField(max_length=100,default='test')
+    nr_telefonu = models.IntegerField(default='1234')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                #default=User.objects.create_user(username=username, password=haslo))
 
 class Rodzic_Uczen(models.Model):
     uczen = models.ForeignKey(Uczen, on_delete=models.CASCADE)
